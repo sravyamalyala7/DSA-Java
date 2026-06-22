4c0b194dc8b4450beba9f0c65eea08e3076c988e
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-22 12:44:23 +0530

    Add anagram check program

---
+++ Strings/AnagramCheck.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if(Arrays.equals(arr1, arr2))
+            System.out.println("Strings are Anagrams");
+        else
+            System.out.println("Strings are Not Anagrams");
+
+        sc.close();
+    }
+}
