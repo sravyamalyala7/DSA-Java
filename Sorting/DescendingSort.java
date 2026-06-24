188c969236979cf6d91d8086ed94dce4222e7a2c
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-24 10:50:03 +0530

    Add Descending Sort algorithm

---
+++ Sorting/DescendingSort.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DescendingSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+
+            for(int j = i + 1; j < n; j++) {
+
+                if(arr[i] < arr[j]) {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array (Descending):");
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
