1f7709b0ab404a680301e576c91a71557a30daec
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-25 15:12:59 +0530

    Add recursive number printing

---
+++ Recursion/PrintNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrintNumbers {
+
+    static void printNumbers(int n) {
+
+        if(n == 0) {
+            return;
+        }
+
+        printNumbers(n - 1);
+
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        printNumbers(n);
+
+        sc.close();
+    }
+}
